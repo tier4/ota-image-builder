53483fc5ba91425047e3eb5cbee7b04bb0de43d9
Bodong Yang <86948717+Bodong-Yang@users.noreply.github.com>
2025-10-01 12:12:38 +0900

    ci: add e2e test for ota-image-builder (#26)
    Add initial e2e test CI for ota-image-builder. This test CI will build the ota-image-builder from current branch, and then use the built binary to generate OTA image from ghcr.io/tier4/ota-client/sys_img_for_test:ubuntu_22.04.
    
    TODO: add otaclient release package into the workflow.

---
+++ docker/e2e_test_base/e2e_test.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -eux
+
+OTA_IMAGE_DIR=/ota-image
+CERT_DIR=/certs
+SYS_IMG_ROOTFS=${SYS_IMG_ROOTFS:-/rootfs}
+
+# gen cert chain
+mkdir -p ${CERT_DIR}
+pushd ${CERT_DIR}
+bash ${CERT_DIR}/gen_cert_chain.sh
+popd
+
+# TODO: add otaclient release package
+
+mkdir -p ${OTA_IMAGE_DIR}
+ota-image-builder -d init \
+    --annotations-file full_annotations.yaml \
+    ${OTA_IMAGE_DIR}
+ota-image-builder -d add-image \
+    --annotations-file full_annotations.yaml \
+    --release-key dev \
+    --sys-config "autoware:sys_config.yaml" \
+    --sys-config "sub:sys_config.yaml" \
+    --rootfs ${SYS_IMG_ROOTFS} \
+    ${OTA_IMAGE_DIR}
+ota-image-builder -d add-image \
+    --annotations-file full_annotations.yaml \
+    --release-key prd \
+    --sys-config "autoware:sys_config.yaml" \
+    --sys-config "sub:sys_config.yaml" \
+    --rootfs ${SYS_IMG_ROOTFS}/var \
+    ${OTA_IMAGE_DIR}
+ota-image-builder -d finalize ${OTA_IMAGE_DIR}
+ota-image-builder -d sign \
+    --sign-cert ${CERT_DIR}/sign.pem \
+    --sign-key ${CERT_DIR}/sign.key \
+    --ca-cert ${CERT_DIR}/test.interm.pem \
+    ${OTA_IMAGE_DIR}
+rm -rf ${CERT_DIR}/*.key
+
+ota-image-tools inspect-index ${OTA_IMAGE_DIR}
