e8f809adf3192a4361c27269c0cb00f408f2ba7d
bodong.yang <bodong.yang@tier4.jp>
2026-02-06 03:10:58 +0000

    finish up scripts for e2e test

---
+++ tests/scripts/e2e_setup_sys_img.sh
@@ -0,0 +1,73 @@
+#!/bin/sh
+# run on alpine based dind image
+set -eux
+
+apk update
+apk add attr
+
+# check if docker is running properly 
+docker info
+
+# ------ empty files ------ #
+# for otaclient PR#492, add a folder that contains lots of empty files
+EMPTY_FILE_COUNTS=5000
+EMPTY_FILE_FOLDER=/empty_files
+
+mkdir ${EMPTY_FILE_FOLDER}
+for i in $(seq 1 ${EMPTY_FILE_COUNTS}); do
+    touch "${EMPTY_FILE_FOLDER}/file_$i.txt"
+done
+
+# ------ xattrs support ------ #
+touch /file_with_xattrs
+setfattr -n user.ota.test -v "test_value" /file_with_xattrs
+
+# ------ large file support ------ #
+dd if=/dev/urandom of=/500M.img bs=1M count=500
+
+# ------ utf-8 support ------ #
+SPECIAL_FILE="path;adf.ae?qu.er\y=str#fragファイルement"
+echo -n "${SPECIAL_FILE}" > "/${SPECIAL_FILE}"
+
+# ------ docker image pull suport ------ #
+BUSYBOX_VER=1.37.0
+docker image pull busybox:${BUSYBOX_VER}
+
+# ------ docker image build support with whiteout files ------ #
+# setup the base image
+BASE_IMAGE_REF=baseimage
+BASE_DOCKERFILE_CONTENTS=$(cat <<EOF
+FROM busybox:${BUSYBOX_VER}
+
+RUN mkdir /lot_of_empty_files; \
+    for i in \$(seq 1 5000); do echo "\$i" > "/lot_of_empty_files/file_\$i.txt"; done; \
+    mkdir -p /dir_with_subdir/subdir; \
+    echo "subdir_file" > /dir_with_subdir/subdir/subdir_files; \
+    mkdir -p /dir_contents_changed/dir_to_be_removed; \
+    mkdir -p /dir_become_file; \
+    echo "used_to_be_a_file" > /file_become_dir
+
+EOF
+)
+# remove/modify files from base image
+UPPER_IMAGE_REF=upperimage
+UPPER_DOCKERFILE_CONTENTS=$(cat <<EOF
+FROM ${BASE_IMAGE_REF}
+
+RUN rm -rf /lot_of_empty_files; \
+    rm -rf /dir_with_subdir; \
+    rm -rf /dir_contents_changed/dir_to_be_removed; \
+    echo "dir_contents_changed" > /dir_contents_changed/new_content; \
+    rm -rf /dir_become_file; echo "i_am_a_file_now" > /dir_become_file; \
+    rm -rf /file_become_dir; mkdir /file_become_dir
+
+EOF
+)
+
+# build base image
+echo "${BASE_DOCKERFILE_CONTENTS}" | docker build -t ${BASE_IMAGE_REF} -
+# build upper image
+echo "${UPPER_DOCKERFILE_CONTENTS}" | docker build -t ${UPPER_IMAGE_REF} -
+
+# inspect the built images
+docker image list
