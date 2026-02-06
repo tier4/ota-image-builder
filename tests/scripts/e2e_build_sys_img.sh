be5818c57204a4cbf1dbfd813bdd4b1cd076953f
bodong.yang <bodong.yang@tier4.jp>
2026-02-06 03:26:55 +0000

    minor fix

--- tests/scripts/e2e_build_sys_img.sh
+++ tests/scripts/e2e_build_sys_img.sh
@@ -1,18 +1,24 @@
 #!/bin/sh
+set -eux
 
 DIND_VER=28-dind
 ROOTFS=dind_rootfs
+SETUP_SCRIPT=$(dirname "$0")/e2e_setup_sys_img.sh
 
 # ------ export the dind image contents ------ #
 mkdir ${ROOTFS}
 docker create --name 28dind_export docker:${DIND_VER}
-docker export | tar -xf - -C ${ROOTFS}
+docker export 28dind_export | tar -xf - -C ${ROOTFS}
+docker rm 28dind_export
 
-cp `which e2e_setup_sys_img.sh` ${ROOTFS}
+cp ${SETUP_SCRIPT} ${ROOTFS}
 
 # ------ use podman to setup the exported system image ------ #
-CONTAINER_REF=setup_dind
-podman run -d --name ${CONTAINER_REF} --privileged \
-    --rootfs ${ROOTFS} /usr/local/bin/dockerd-entrypoint.sh
+podman run -d --replace --name setup_dind --privileged \
+    --rootfs `realpath ${ROOTFS}` /usr/local/bin/dockerd-entrypoint.sh
+# NOTE: wait for the dockerd fully starts up
+sleep 16
 
-podman exec ${CONTAINER_REF} /e2e_setup_sys_img.sh
+podman exec setup_dind /e2e_setup_sys_img.sh
+podman stop setup_dind
+podman rm setup_dind
