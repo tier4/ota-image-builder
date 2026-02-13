5c86da3dc20cf01fcd1e29d94d600e0cb1a2a8f2
bodong.yang <bodong.yang@tier4.jp>
2026-02-13 14:48:13 +0000

    pack_artifact: use lib functions from ota-image-libs

--- src/ota_image_builder/cmds/pack_artifact.py
+++ src/ota_image_builder/cmds/pack_artifact.py
@@ -15,13 +15,10 @@
 from __future__ import annotations
 
 import logging
-import os
-import shutil
 from pathlib import Path
 from typing import TYPE_CHECKING
-from zipfile import ZIP_STORED, ZipFile, ZipInfo
 
-from ota_image_libs.v1.consts import IMAGE_INDEX_FNAME, INDEX_JWT_FNAME
+from ota_image_libs.v1.artifact.packer import pack_artifact
 from ota_image_libs.v1.image_index.utils import ImageIndexHelper
 
 from ota_image_builder._common import check_if_valid_ota_image, exit_with_err_msg
@@ -32,86 +29,7 @@ if TYPE_CHECKING:
 
 logger = logging.getLogger(__name__)
 
-REPORT_BATCH_SIZE = 10_000
-DEFAULT_TIMESTAMP = (2009, 1, 1, 0, 0, 0)
-RW_CHUNK_SIZE = 8 * 1024 * 1024  # 8MiB
-
-
-def _add_dir(zipf: ZipFile, filename: Path, arcname: Path | str) -> None:
-    """
-    Add a directory to the OTA image zipfile. The src must be a directory.
-    """
-    _zipinfo = ZipInfo.from_file(
-        filename=f"{str(filename).rstrip('/')}/",
-        arcname=f"{str(arcname).rstrip('/')}/",
-    )
-    _zipinfo.CRC = 0
-    _zipinfo.date_time = DEFAULT_TIMESTAMP
-    zipf.mkdir(_zipinfo, mode=0o755)
-
-
-def _add_file(zipf: ZipFile, filename: Path, arcname: Path | str) -> None:
-    """
-    Add a regular file to the OTA image zipfile. The src must be a regular file.
-
-    Basically a copy of the ZipFile.writestr method.
-    """
-    _zipinfo = ZipInfo.from_file(filename=filename, arcname=str(arcname))
-    _zipinfo.date_time = DEFAULT_TIMESTAMP
-    _zipinfo.compress_type = zipf.compression
-    _zipinfo.compress_level = zipf.compresslevel
-    _zipinfo.external_attr |= 0o644 << 16  # rw_r_r_
-
-    with open(filename, "rb") as src, zipf.open(_zipinfo, "w") as dst:
-        shutil.copyfileobj(src, dst, RW_CHUNK_SIZE)
-
-
-def _pack_artifact(_image_root: Path, _output: Path):
-    _file_count, _top_level = 0, True
-    with ZipFile(_output, mode="w", compression=ZIP_STORED) as output_f:
-        for curdir, _, files in os.walk(_image_root):
-            curdir = Path(curdir)
-            relative_curdir = curdir.relative_to(_image_root)
-
-            if _top_level:
-                _top_level = False
-
-                # add the index.json file as the first file entry in zipfile,
-                #   effectively defining the manifest for this image.
-                # see https://pkware.cachefly.net/webdocs/casestudies/APPNOTE.TXT chapter 4.1.11
-                #   for more details about ZIP manifest.
-                _add_file(
-                    zipf=output_f,
-                    filename=curdir / IMAGE_INDEX_FNAME,
-                    arcname=IMAGE_INDEX_FNAME,
-                )
-                _file_count += 1
-
-                # following the index.json, add the index.jwt file as the second file entry
-                _add_file(
-                    zipf=output_f,
-                    filename=curdir / INDEX_JWT_FNAME,
-                    arcname=INDEX_JWT_FNAME,
-                )
-                _file_count += 1
-
-                for _fname in sorted(files):
-                    if _fname in (IMAGE_INDEX_FNAME, INDEX_JWT_FNAME):
-                        continue
-                    _add_file(zipf=output_f, filename=curdir / _fname, arcname=_fname)
-                    _file_count += 1
-
-            else:
-                _add_dir(zipf=output_f, filename=curdir, arcname=relative_curdir)
-                for _file in sorted(files):
-                    _src = curdir / _file
-                    _relative_src = relative_curdir / _file
-                    _add_file(zipf=output_f, filename=_src, arcname=_relative_src)
-                    _file_count += 1
-                    if _file_count % REPORT_BATCH_SIZE == 0:
-                        print(
-                            f"Packing in-progress: {_file_count} files are packed ..."
-                        )
+RW_CHUNK_SIZE = 8 * 1024**2  # 8MiB
 
 
 def pack_artifact_cmd_args(
@@ -160,7 +78,7 @@ def pack_artifact_cmd(args: Namespace) -> None:
         exit_with_err_msg(f"{image_root} doesn't hold a valid OTA image.")
 
     try:
-        _pack_artifact(image_root, output)
+        pack_artifact(image_root, output, rw_chunk_size=RW_CHUNK_SIZE)
         print(f"Packed artifact is populated to {output}.")
     except Exception as e:
         _err_msg = (
